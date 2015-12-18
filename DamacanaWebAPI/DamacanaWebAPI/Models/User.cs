414936ed8a07e83aa525b52079896f8912558db3
emredog <emredog@gmail.com>
2015-12-18 11:12:08 +0200

    Cleaned-up default authentication stuff
    Forgot to uncheck authentication checkbox while creating the project in
    the first place

--- DamacanaWebAPI/DamacanaWebAPI/Models/User.cs
+++ DamacanaWebAPI/DamacanaWebAPI/Models/User.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace DamacanaWebAPI.Models
 {
     public class User
-    {
+    { 
         public Guid Id { get; set; }
+        [Required]
         public String Name { get; set; }
+        [Required]
         public String Surname { get; set; }
         public String Address { get; set; }
         // TODO
