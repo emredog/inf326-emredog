87e2c997a6461a84e45807279eb70f2e46aeafb8
emredog <emredog@gmail.com>
2015-12-20 15:11:11 +0200

    Product controller added with only GET endpoints

---
+++ DamacanaWebAPI/DamacanaWebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DamacanaWebAPI.Models;
+
+namespace DamacanaWebAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private DamacanaWebAPIContext db = new DamacanaWebAPIContext();
+
+        // GET: api/Products
+        public IQueryable<Product> GetProducts()
+        {
+            return db.Products;
+        }
+
+        // GET: api/Products/5
+        [ResponseType(typeof(Product))]
+        public async Task<IHttpActionResult> GetProduct(Guid id)
+        {
+            Product product = await db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+    }
+}
