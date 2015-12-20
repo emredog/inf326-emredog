c6bbced98f5f202e21db0acc7e1fb43d7d2a2e00
emredog <emredog@gmail.com>
2015-12-20 14:31:04 +0200

    clean start
    need to recreate the project without authentication

--- DamacanaWebAPI/DamacanaWebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DamacanaWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- DamacanaWebAPI/DamacanaWebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DamacanaWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
