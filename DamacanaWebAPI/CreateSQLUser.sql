850ccf26fd879f461a94bba6afcf54b91abe4ffa
emredog <emredog@gmail.com>
2015-12-21 11:24:13 +0200

    web config is configured to use local SQL Express
    I had to create another user for controllers to access the database. see
    CreateSQLUser.sq

---
+++ DamacanaWebAPI/CreateSQLUser.sql
@@ -0,0 +1,9 @@
+﻿-- Create user for SQL Authentication
+CREATE LOGIN myuser WITH PASSWORD = '123qweasd'
+,DEFAULT_DATABASE = DamacanaWebAPIContext
+GO
+-- Add User to first database
+USE DamacanaWebAPIContext;
+CREATE USER myuser FOR LOGIN myuser;
+EXEC sp_addrolemember 'db_datareader', 'myuser'
+EXEC sp_addrolemember 'db_datawriter', 'myuser'
